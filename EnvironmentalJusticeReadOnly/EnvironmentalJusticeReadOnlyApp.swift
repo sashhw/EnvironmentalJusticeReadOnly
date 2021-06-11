71e69e0dad77034949e9540e21d371e784f9626b
sashhw <sash.walk@gmail.com>
2021-06-11 11:05:57 -0500

    Initial Commit

---
+++ EnvironmentalJusticeReadOnly/EnvironmentalJusticeReadOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentalJusticeReadOnlyApp.swift
+//  EnvironmentalJusticeReadOnly
+//
+//  Created by Sasha on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentalJusticeReadOnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

5bad62873d056f95d0a0fa8f56fc7a05afd57393
sashhw <sash.walk@gmail.com>
2022-04-12 10:07:03 -0500

    4/12

--- EnvironmentalJusticeReadOnly/View/ContentView.swift
+++ EnvironmentalJusticeReadOnly/View/ContentView.swift
@@ -9,149 +9,160 @@ import SwiftUI
 
 struct ContentView: View {
     @State var flipped = false
-    let usCases = USCases()
-    @State var showSignInForm = false
     @State var showSheetView = false
+    let usCases = USCases()
     let screenRect = UIScreen.main.bounds
+    let ejTitleText = ejTitle()
     var body: some View {
-        HStack {
-            NavigationView {
-                VStack(alignment: .center) {
-                    ZStack {
-                        VStack {
-                            ZStack {
-                                Image("spainEnv")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                                    .frame(width: screenRect.width, height: 1500, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .cornerRadius(5)
-                                    .shadow(color: .black, radius: 4)
-                                    .opacity(0.6)
-                                    .overlay(
-                                        
-                                        Group {
-                                            Spacer()
-                                            ejTitle()
-                                                .padding(.bottom, 100)
-                                            
-                                            if !self.flipped {
-                                                Text("Info")
-                                                    .kerning(2.0)
-                                                    .fontWeight(.light)
-                                                    .padding()
-                                                    .font(.system(.headline))
-                                                    .foregroundColor(.black)
-                                                    .multilineTextAlignment(.center)
-                                                    .scaleEffect(0.9)
-                                                    .background(Color.white)
-                                                    .cornerRadius(15)
-                                                    .opacity(0.7)
-                                                    .padding(.horizontal, 30.0)
-                                                    .minimumScaleFactor(0.5)
-                                                    .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
-                                            } else {
-                                                Text(ejDef)
-                                                    .fontWeight(.light)
-                                                    .padding()
-                                                    .font(.system(.headline))
-                                                    .foregroundColor(.black)
-                                                    .multilineTextAlignment(.center)
-                                                    .scaleEffect(0.9)
-                                                    .background(Color.white)
-                                                    .cornerRadius(15)
-                                                    .opacity(0.7)
-                                                    .padding(.horizontal, 30.0)
-                                                    .minimumScaleFactor(0.5)
-                                                    .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
-                                            }
-                                            NavigationLink(destination: CardView(card: Card(word: "", definition: ""))) {
-                                                Text("Terms")
-                                                    .kerning(2.0)
-                                                    .fontWeight(.light)
-                                                    .padding()
-                                                    .font(.system(.headline))
-                                                    .foregroundColor(.black)
-                                                    .multilineTextAlignment(.center)
-                                                    .scaleEffect(0.9)
-                                                    .background(Color.white)
-                                                    .cornerRadius(15)
-                                                    .opacity(0.7)
-                                                    .padding(.horizontal, 30.0)
-                                                    .minimumScaleFactor(0.5)
-                                                    .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
-                                            }
-                                            
-                                            NavigationLink(destination: usCases) {
-                                                
-                                                Text("Cases")
-                                                    .kerning(2.0)
-                                                    .fontWeight(.light)
-                                                    .padding()
-                                                    .font(.system(.headline))
-                                                    .foregroundColor(.black)
-                                                    .multilineTextAlignment(.center)
-                                                    .scaleEffect(0.9)
-                                                    .background(Color.white)
-                                                    .cornerRadius(15)
-                                                    .opacity(0.7)
-                                                    .padding(.horizontal, 30.0)
-                                                    .minimumScaleFactor(0.5)
-                                                    .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
-                                            }
-                                            
-                                            NavigationLink(destination: MapViewThree()) {
-                                                Text("Map")
-                                                    .kerning(2.0)
-                                                    .fontWeight(.light)
-                                                    .padding()
-                                                    .font(.system(.headline))
-                                                    .foregroundColor(.black)
-                                                    .multilineTextAlignment(.center)
-                                                    .scaleEffect(0.9)
-                                                    .background(Color.white)
-                                                    .cornerRadius(15)
-                                                    .opacity(0.7)
-                                                    .padding(.horizontal, 30.0)
-                                                    .minimumScaleFactor(0.5)
-                                                    .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
-                                            }
-                                            .padding(.bottom, 200.0)
-                                            
-                                            .navigationBarItems(trailing:
-                                                                    Button(action: {
-                                                self.showSheetView.toggle()
-                                                
-                                            }
-                                                                          )
-                                                                {
-                                                Image(systemName: "info.circle")
-                                                    .font(Font.system(.title2))
-                                                    .foregroundColor(.black)
-                                                    .padding(.leading)
-                                                    .opacity(0.5)
-                                            }
-                                            )
-                                            Spacer()
+        NavigationView {
+            ZStack {
+                Image("spainEnv")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: screenRect.width, height: 1500, alignment: .center)
+                    .cornerRadius(5)
+                    .shadow(color: .black, radius: 4)
+                    .opacity(0.6)
+                    .overlay(
+                        Group {
+                            Spacer()
+                            
+                            VStack {
+                                ejTitleText
+                                    .padding(.bottom, 100)
+                                
+                                Button {
+                                    flipped.toggle()
+                                } label: {
+                                    Text("Info")
+                                        .kerning(2.0)
+                                        .fontWeight(.light)
+                                        .padding()
+                                        .font(.system(.headline))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                        .scaleEffect(0.9)
+                                        .background(Color.white)
+                                        .cornerRadius(15)
+                                        .opacity(0.7)
+                                        .padding(.horizontal, 30.0)
+                                        .minimumScaleFactor(0.5)
+                                        .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
+                                }
+                                
+                                if flipped {
+                                    ZStack {
+                                        Text(ejDef)
+                                            .fontWeight(.light)
+                                            .padding()
+                                            .font(.system(.headline))
+                                            .foregroundColor(.black)
+                                            .multilineTextAlignment(.center)
+                                            .scaleEffect(0.9)
+                                            .background(Color.white)
+                                            .cornerRadius(15)
+                                            .opacity(0.7)
+                                            .padding(.horizontal, 30.0)
+                                            .minimumScaleFactor(0.5)
+                                            .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
+                                        Button {
+                                            flipped.toggle()
+                                        } label: {
+                                            Text("Dismiss")
+                                                .kerning(2.0)
+                                                .fontWeight(.light)
+                                                .font(.caption)
+                                                .bold()
+                                                .foregroundColor(.black)
+                                                .multilineTextAlignment(.center)
+                                                .border(Color.black, width: 0.5)
+                                                .scaleEffect(0.9)
+                                                .cornerRadius(15)
+                                                .padding(.top, 450)
+                                                .padding(.leading, 200)
+//                                                .padding(50)
                                         }
-                                            .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                                            .animation(.default)
-                                            .onTapGesture {
-                                                self.flipped.toggle()
-                                            }
-                                        , alignment: .center
-                                    )
+                                    }
+                                }
+                                
+                                NavigationLink(destination: CardView(card: Card(word: "", definition: ""))) {
+                                    Text("Terms")
+                                        .kerning(2.0)
+                                        .fontWeight(.light)
+                                        .padding()
+                                        .font(.system(.headline))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                        .scaleEffect(0.9)
+                                        .background(Color.white)
+                                        .cornerRadius(15)
+                                        .opacity(0.7)
+                                        .padding(.horizontal, 30.0)
+                                        .minimumScaleFactor(0.5)
+                                        .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
+                                }
+                                
+                                NavigationLink(destination: usCases) {
+                                    
+                                    Text("Cases")
+                                        .kerning(2.0)
+                                        .fontWeight(.light)
+                                        .padding()
+                                        .font(.system(.headline))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                        .scaleEffect(0.9)
+                                        .background(Color.white)
+                                        .cornerRadius(15)
+                                        .opacity(0.7)
+                                        .padding(.horizontal, 30.0)
+                                        .minimumScaleFactor(0.5)
+                                        .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
+                                }
+                                
+                                NavigationLink(destination: MapViewThree()) {
+                                    Text("Map")
+                                        .kerning(2.0)
+                                        .fontWeight(.light)
+                                        .padding()
+                                        .font(.system(.headline))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                        .scaleEffect(0.9)
+                                        .background(Color.white)
+                                        .cornerRadius(15)
+                                        .opacity(0.7)
+                                        .padding(.horizontal, 30.0)
+                                        .minimumScaleFactor(0.5)
+                                        .frame(width: UIScreen.main.bounds.width-20, alignment: .center)
+                                }
                             }
+                            .padding(.bottom, 200.0)
+                            
+                            .navigationBarItems(trailing:
+                                                    Button(action: {
+                                self.showSheetView.toggle()
+                            }){
+                                Image(systemName: "info.circle")
+                                    .font(Font.system(.caption))
+                                    .foregroundColor(.black)
+                                    .padding(.leading)
+                                    .opacity(0.5)
+                            }
+                            )
+                            Spacer()
                         }
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                //                .sheet(isPresented: $showSignInForm) {
-                //                }
-                .sheet(isPresented: $showSheetView) {
-                    CiteSheetView()
-                }
+                            .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                            .animation(.default)
+                        
+                        , alignment: .center
+                    )
             }
+        }
+        .frame(maxWidth: .infinity)
+        .sheet(isPresented: $showSheetView) {
+            CiteSheetView()
         }
     }
 }
